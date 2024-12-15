9d34847e5c7b3f9fdac5a65fbb1d88b8decd137c
Bright Chen <1024347104@qq.com>
2024-12-15 18:53:14 +0800

    Site updated: 2024-12-15 18:53:13

--- js/myself.js
+++ js/myself.js
@@ -1,6 +1,7 @@
 $(document).ready(function (e) {
     $('#footer-wrap').append('<div class="framework-info"></div>')
     show_date_time();
+    showIcp();
 })
 
 var pastTimeString = "2024-12-13T18:00:00"; // 格式：YYYY-MM-DDTHH:mm:ss
@@ -37,4 +38,21 @@ function show_date_time() {
     var timeDifference = calculateTimeDifference();
     $('.framework-info').html('安全运行<span>' + timeDifference + '</span>');
     window.setTimeout("show_date_time()", 1000);
+}
+
+
+function  showIcp(){
+
+    // 获取当前域名和路径 指定 域名下首页展示备案信息
+    const hostname = window.location.hostname;
+    const isHomePage = typeof GLOBAL_CONFIG_SITE !== 'undefined' && GLOBAL_CONFIG_SITE.isHome;
+    console.log('hostname');
+    // 检查是否满足条件
+    if (hostname === '192.168.31.229' && isHomePage) {
+        // 显示备案信息的逻辑  nofollow 告知搜索引擎不要追踪此链接或传递权重
+        $('#footer-wrap').append('<a href="http://beian.miit.gov.cn/" rel="nofollow" style="" target="_blank">XICP备xxxxxxx-x</a>')
+    } else {
+        console.log('当前页面不符合条件');
+    }
+
 }
